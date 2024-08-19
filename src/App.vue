1fea7a7a2b283b2ff770f65c56490224bbcac206
Eom Juntae <junbrian9815@gmail.com>
2024-08-19 19:33:23 +0900

    컴포넌트 분리

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,43 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <ToDoList/>
+  <AddTodo @add-todo="addTodo"/>
+  <ToDoList :todos="todos" @remove-todo="removeTodo" @save-todos="saveTodos"/>
 </template>
 
 <script>
+import AddTodo from './components/AddTodo.vue';
 import ToDoList from './components/ToDoList.vue'
 
 export default {
   name: 'App',
   components: {
+    AddTodo,
     ToDoList
+  },
+  data() {
+    return {
+      todos: []
+    }
+  },
+  mounted() {
+    const savedTodos = localStorage.getItem('todos');
+    if(savedTodos) {
+      this.todos = JSON.parse(savedTodos);
+    }
+  },
+  methods: {
+    addTodo(newTodo) {
+      this.todos.push({text: newTodo, complted: false});
+      this.saveTodos();
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+      this.saveTodos();
+    },
+    saveTodos() {
+      // 현재 할 일 목록을 로컬 스토리지에 저장합니다. 
+      localStorage.setItem('todos', JSON.stringify(this.todos))
+    }
   }
 }
 </script>
